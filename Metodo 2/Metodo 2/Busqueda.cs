c8c6d3bee18756d40aa81768975f1dfba399e8b5
LuisEstrada33 <43280434+LuisEstrada33@users.noreply.github.com>
2018-12-03 09:11:43 -0800

    Add files via upload

---
+++ Metodo 2/Metodo 2/Busqueda.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo_2
+{
+    class Busqueda
+    {
+        public bool BusquedaSecuencial(int Elemento, List<int> Lista)
+        {
+            int Tamaño = Lista.Count;
+            int Posicion = 0;
+
+            while (Posicion < Tamaño)
+            {
+                if (Lista[Posicion] == Elemento)
+                {
+                    return true;
+                }
+                else
+                {
+                    Posicion++;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Metodo 2/Metodo 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Busqueda Buscar = new Busqueda();
+
+            List<int> Lista = new List<int>();
+            
+            Lista.Add(10);
+            Lista.Add(6);
+            Lista.Add(8);
+            Lista.Add(15);
+            Lista.Add(4);
+            Lista.Add(20);
+            Lista.Add(9);
+            Lista.Add(30);
+            Lista.Add(1);
+
+            Console.Write("Que dato desea buscar?: " );
+            int Elemento = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("BUSQUEDA SECUENCIAL");
+            Console.WriteLine();
+            Console.WriteLine("El elemento " + Elemento);
+            Console.WriteLine("¿se encuentra en la lista? " + Buscar.BusquedaSecuencial(Elemento, Lista));
+            Console.ReadKey();
+        }
+    }
+}
