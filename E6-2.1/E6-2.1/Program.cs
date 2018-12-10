426bd9010e2c871a1a1263e5ad6c7a11bdb3f29f
LuisEstrada33 <43280434+LuisEstrada33@users.noreply.github.com>
2018-12-10 15:55:23 -0800

    Add files via upload

---
+++ E6-2.1/E6-2.1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E6_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, puntero = 0, final = 11, mitad = -999;
+            int[] vec = { 3, 5, 8, 10, 100, 11, 80, 3, 205, 7, 159, 36 };
+            bool encontro = false;
+
+            Console.Write("Ingrese un numero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            while(!encontro && puntero<=final)
+            {
+                mitad = (int)((puntero + final) / 2);
+                if (numero == vec[mitad])
+                    encontro = true;
+
+                else if (numero < vec[mitad])
+                    final = mitad - 1;
+                else
+                    puntero = mitad + 1;                   
+            }
+            if (encontro)
+                Console.Write("El dato se encuentra y esta en la posicion: " + (mitad + 1));
+            else
+                Console.Write("El dato no se encuentra");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ E6-2.2/E6-2.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using E6_2._2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BusquedaBinaria
+{
+    class Busqueda
+    {
+        static void Main(string[] args)
+        {
+            Binaria pv = new E6_2._2.Binaria();
+            pv.Cargar();
+            pv.Imprimir();
+            Console.Write("\n\nElemento a buscar: ");
+            int num = int.Parse(Console.ReadLine());
+            pv.busqueda(num);
+            Console.ReadKey();
+
+
+        }
+    }
+}
